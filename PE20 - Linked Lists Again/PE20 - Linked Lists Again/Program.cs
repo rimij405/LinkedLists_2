108db3fc7ec63e0521c9ecc47c8b4488433e1a08
Ian Effendi <effendiian+github@gmail.com>
2016-04-24 13:32:16 -0400

    Overview: Linked List base for the PE20 assignment being set up.

---
+++ PE20 - Linked Lists Again/PE20 - Linked Lists Again/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE20___Linked_Lists_Again
+{
+	// test class
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// create a linked list
+			LinkedList list = new LinkedList();
+
+			// populate the list
+			list.Add("This");
+			list.Add("is");
+			list.Add("a");
+			list.Add("test");
+			list.Add("of");
+			list.Add("a");
+			list.Add("linked");
+			list.Add("list");
+
+			// traverse the list
+			list.Traverse();
+		}
+	}
+}
