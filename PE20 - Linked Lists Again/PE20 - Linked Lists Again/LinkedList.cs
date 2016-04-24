b303f31185bc8c8a32a0c7a191a5086dad9b3641
Ian Effendi <effendiian+github@gmail.com>
2016-04-24 15:49:55 -0400

    Overview: Completed and debugged features for: InsertSorted, Add, GetData, and Insert, methods.

--- PE20 - Linked Lists Again/PE20 - Linked Lists Again/LinkedList.cs
+++ PE20 - Linked Lists Again/PE20 - Linked Lists Again/LinkedList.cs
@@ -13,6 +13,44 @@ namespace PE20___Linked_Lists_Again
 		private Node head = null; // initially nothing in the list
 		private int count = 0;
 
+		// get data from an item in the list.
+		public string GetData(int index)
+		{
+			// If index is invalid or list is empty.
+			if (index < 0 || index >= count || head == null)
+			{
+				return null;
+			}
+			// If index is first index.
+			else if (index == 0)
+			{
+				return head.Data;
+			}
+			// If index is within bounds.
+			else
+			{
+				Node current = head;
+
+				// Loop through the LinkedList.
+				for (int i = 0; i < index; i++)
+				{
+					// If the next node is NOT null.
+					if (current.Next != null)
+					{
+						// Move to the next node in the list.
+						current = current.Next;
+					}
+					// If the next node is null.
+					else
+					{
+						return null;
+					}
+				}
+
+				return current.Data;
+			}
+		}
+
 		// add an item to the list
 		public void Add(string str)
 		{
@@ -38,6 +76,133 @@ namespace PE20___Linked_Lists_Again
 			// now at end of the list
 			current.Next = newNode; // add the new node at the end of the list
 			count++;
+		}
+
+		// insert a new item, before the selected item.
+		public void Insert(string data, int index)
+		{
+			// If list is empty or if index is greater than or equal to the last index.
+			if (head == null || index >= (count - 1))
+			{
+				// Add.
+				Add(data);
+				return;
+			}
+			// If the index is less than or equal to the first index.
+			else if (index <= 0)
+			{
+				Node insert = new Node(data);
+				insert.Next = head;
+				head = insert;
+				count++;
+				return;
+			}
+			// If index is within bounds.
+			else
+			{
+				// Loop through to the node previous (index - 1).
+				Node previous = head;
+				for (int i = 0; i < (index - 1); i++)
+				{
+					// If the next node is NOT null.
+					if (previous.Next != null)
+					{
+						// Move to the next node in the list.
+						previous = previous.Next;
+					}
+					else
+					{
+						// At this point, you've reached the end. Just add.
+						Add(data);
+						return;
+					}
+				}
+
+				// Create new node.
+				Node insert = new Node(data);
+
+				// Assign the previous node's temp.Next to temp.
+				insert.Next = previous.Next;
+
+				// Reassign the previous node's next to new node.
+				previous.Next = insert;
+
+				// Increment the count.
+				count++;
+			}
+		}
+
+		// insert a new item, before the selected item.
+		public void InsertSorted(string data)
+		{
+			// If list is empty.
+			if (head == null)
+			{
+				// Add.
+				Add(data);
+				return;
+			}
+			else if (String.Compare(data, head.Data) < 0)
+			{
+				Node insert = new Node(data);
+				insert.Next = head;
+				head = insert;
+				count++;
+				return;
+			}
+			// If list is not empty.
+			else
+			{
+				Node current = head;
+
+				while (current.Next != null)
+				{
+					if (String.Compare(data, current.Next.Data) < 0)
+					{
+						// Insert before the current.
+						Node insert = new Node(data);
+						insert.Next = current.Next;
+						current.Next = insert;
+						count++;
+						return;
+					}
+					else if (String.Compare(data, current.Next.Data) > 0)
+					{
+						// Go to the next node.
+						current = current.Next;
+					}
+					else //if (String.Compare(data, current.Next.Data) == 0)
+					{
+						// Go to the next node.
+						current = current.Next;
+						
+						// Insert the node.
+						Node insert = new Node(data);
+						insert.Next = current.Next;
+						current.Next = insert;
+						count++;
+						return;
+					}
+				}
+
+				// If it hasn't been returned, insert it here.
+				// Insert the node.
+				Node insertLast = new Node(data);
+				insertLast.Next = current.Next;
+				current.Next = insertLast;
+				count++;
+				return;
+			}
+		}
+
+		// clear the list, removing all data.
+		public void Clear()
+		{
+			// remove all references to any data.
+			head = null; // Other nodes will be handled by garbage collection.
+
+			// reset the count.
+			count = 0;
 		}
 
 		// traverse the list, printing all data
--- PE20 - Linked Lists Again/PE20 - Linked Lists Again/Program.cs
+++ PE20 - Linked Lists Again/PE20 - Linked Lists Again/Program.cs
@@ -15,17 +15,29 @@ namespace PE20___Linked_Lists_Again
 			LinkedList list = new LinkedList();
 
 			// populate the list
-			list.Add("This");
-			list.Add("is");
-			list.Add("a");
-			list.Add("test");
-			list.Add("of");
-			list.Add("a");
-			list.Add("linked");
-			list.Add("list");
+			Console.WriteLine("InsertSorted: { f, b, a, d, g }");
+			list.InsertSorted("f");
+			list.InsertSorted("b");
+			list.InsertSorted("a");
+			list.InsertSorted("d");
+			list.InsertSorted("g");
 
 			// traverse the list
 			list.Traverse();
+			Console.ReadLine();
+
+			// add some more.
+			Console.WriteLine("InsertSorted: { c, h, e }");
+			list.InsertSorted("c");
+			list.InsertSorted("h");
+			list.InsertSorted("e");
+
+			// traverse the list
+			list.Traverse();
+			Console.ReadLine();
+
+			Console.WriteLine("Get Data from element #5 (index 4): " + list.GetData(4));
+			Console.ReadLine();
 		}
 	}
 }
